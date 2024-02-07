023273bd8ffddee1f2ac8cbba24b645da156b5be
Keith Tang <pokpok9301@gmail.com>
2024-02-07 11:20:56 +0000

    add while practices

---
+++ src/main/scala/exercises/controls/while_practice.scala
@@ -0,0 +1,21 @@
+package exercises.controls
+
+object while_practice{
+  def main (args: Array[String]): Unit = {
+      var x = 0;
+      // while loop
+      while (x < 10) {
+        println(s"x = $x")
+        x += 1;
+      }
+
+      // do while loop
+      var y = 9;
+      do {
+        println(s"y = $y")
+        y -= 1;
+      } while (y >= 0)
+
+  }
+
+}
